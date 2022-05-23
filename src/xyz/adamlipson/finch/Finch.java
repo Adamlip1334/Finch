9e6fb686b4c0e354e2c42f33136e1279dc92e133
Adam Lipson <40801946+Adamlip1334@users.noreply.github.com>
2022-05-23 12:27:58 -0400

    Added Interpreter

--- src/xyz/adamlipson/finch/Finch.java
+++ src/xyz/adamlipson/finch/Finch.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Finch {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -25,6 +27,7 @@ public class Finch {
         run(new String(bytes, Charset.defaultCharset()));
         // Indicate an error in the exit code.
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
 
@@ -51,12 +54,18 @@ public class Finch {
         // Stop if there was a syntax error.
         if (hadError) return;
 
+        interpreter.interpret(expression);
+
         System.out.println(new AstPrinter().print(expression));
     }
     static void error(int line, String message) {
         report(line, "", message);
     }
-
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
+    }
     private static void report(int line, String where,
                                String message) {
         System.err.println(
