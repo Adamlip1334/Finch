73492fd9b0f4ea51e30a7bb99998ad17b6cc8575
Adam Lipson <lipsonadam13@gmail.com>
2022-08-05 19:28:19 -0400

    Update Finch.java
    fixed typo

--- src/xyz/adamlipson/finch/Finch.java
+++ src/xyz/adamlipson/finch/Finch.java
@@ -14,7 +14,7 @@ public class Finch {
     static boolean hadRuntimeError = false;
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
-            System.out.println("Usage: jlox [script]");
+            System.out.println("Usage: Finch");
             System.exit(64);
         } else if (args.length == 1) {
             runFile(args[0]);
